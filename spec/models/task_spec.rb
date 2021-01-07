99afbd4acef0bbdecb9ee830ead0d4cc4373bd78
mikiya_endo <olive_miu@mikiya-endonoMacBook-Air.local>
2021-01-07 23:06:27 +0900

    [fix] モデルスペックでFactoryBotを省略できるよう設定

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,27 +4,27 @@ RSpec.describe Task, type: :model do
 
   describe "validation" do
     it "is valid with all attributes" do
-      expect(FactoryBot.build(:task)).to be_valid
+      expect(build(:task)).to be_valid
     end
     it "is invalid without a title" do
-      task_without_title = FactoryBot.build(:task, title: nil)
+      task_without_title = build(:task, title: nil)
       expect(task_without_title).to be_invalid
       expect(task_without_title.errors[:title]).to include("can't be blank")
     end
     it "is invalid without a status" do
-      task_without_status = FactoryBot.build(:task, status: nil)
+      task_without_status = build(:task, status: nil)
       expect(task_without_status).to be_invalid
       expect(task_without_status.errors[:status]).to include("can't be blank")
     end
     it "is invalid with a duplicate title" do
-      FactoryBot.create(:task, title: "rspec_test")
-      task_with_duplicated_title = FactoryBot.build(:task, title: "rspec_test")
+      create(:task, title: "rspec_test")
+      task_with_duplicated_title = build(:task, title: "rspec_test")
       expect(task_with_duplicated_title).to be_invalid
       expect(task_with_duplicated_title.errors[:title]).to include("has already been taken")
     end
     it "is valid with another title" do
-      task1 = FactoryBot.create(:task)
-      task_with_another_title = FactoryBot.build(:task)
+      task1 = create(:task)
+      task_with_another_title = build(:task)
       expect(task_with_another_title).to be_valid
     end
   end
