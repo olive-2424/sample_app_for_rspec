60d43adf5fe1847754d7be77c16f6b652ae3c6e1
mikiya_endo <olive_miu@mikiya-endonoMacBook-Air.local>
2021-01-08 17:47:41 +0900

    [fix] taskテストデータのstatusカラムをenum形式で記載

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     sequence(:title) { |n| "rspec_test_#{n}" }
     content { "content" }
-    status   { 'todo' }
+    status   { :todo }
     deadline { 1.week.from_now }
     association :user
   end
