6a91046ea85ed0385005d534d9c4f6c389bd3fbe
mikiya_endo <olive_miu@mikiya-endonoMacBook-Air.local>
2021-01-09 13:39:36 +0900

    [add] capybara,webdriversの導入、ユーザーの新規登録が正常の値の時、成功、emailが未入力のとき、emailが登録済みのメールアドレスのとき失敗するテストを記載

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  let(:user) { create(:user) }
+  describe 'ログイン前' do
+    describe 'ユーザーの新規登録' do
+      context 'フォームの入力値が正常' do
+        it 'ユーザーの新規作成が成功する' do
+          visit sign_up_path
+          fill_in 'Email', with: 'test@example.com'
+          fill_in 'Password', with: 'password'
+          fill_in 'Password confirmation', with: 'password'
+          click_button 'SignUp'
+          expect(current_path).to eq login_path
+          expect(page).to have_content 'User was successfully created.'
+        end
+      end
+
+      context 'メールアドレスが未入力' do
+        it 'ユーザーの新規作成が失敗する' do
+          visit sign_up_path
+          fill_in 'Email', with: nil
+          fill_in 'Password', with: 'password'
+          fill_in 'Password confirmation', with: 'password'
+          click_button 'SignUp'
+          expect(current_path).to eq users_path
+          expect(page).to have_content "Email can't be blank"
+        end
+      end
+
+      context '登録済みのメールアドレスを使用' do
+        it 'ユーザーの新規作成が失敗する' do
+          visit sign_up_path
+          fill_in 'Email', with: user.email
+          fill_in 'Password', with: 'password'
+          fill_in 'Password confirmation', with: 'password'
+          click_button 'SignUp'
+          expect(current_path).to eq users_path
+          expect(page).to have_content 'Email has already been taken'
+        end
+      end
+    end
+
+    describe 'マイページ' do
+      context 'ログインしていない状態' do
+        it 'マイページへのアクセスが失敗する' do
+        end
+      end
+    end
+  end
+
+  describe 'ログイン後' do
+    describe 'ユーザー編集' do
+      context 'フォームの入力値が正常' do
+        it 'ユーザーの編集が成功する'
+      end
+
+      context 'メールアドレスが未入力' do
+        it 'ユーザーの編集が失敗する' 
+      end
+
+      context '登録済のメールアドレスを使用' do
+        it 'ユーザーの編集が失敗する'
+      end
+
+      context '他ユーザーの編集ページにアクセス' do
+        it '編集ページへのアクセスが失敗する'
+      end
+    end
+
+    describe 'マイページ' do
+      context 'タスクを作成' do
+        it '新規作成したタスクが表示される'
+      end
+    end
+  end
+
+
+
+  before do
+    driven_by(:rack_test)
+  end
+
+end
